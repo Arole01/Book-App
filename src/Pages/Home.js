b7be0774253fdd04043e50efbb95278fae324a45
Arole01 <ajosedavidayobami@gmail.com>
2026-03-24 13:52:46 +0000

    homeCss1

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState} from "react";
 import { Link } from "react-router-dom";
+import "./Home.css";
 // import axios from "axios";
 // axios.defaults.adapter = require('axios/lib/adapters/xhr');
 
